c2e05d4fbec5dd4d64976a09ade14d0ff27a572c
melkeades <118681335+melkeades@users.noreply.github.com>
2023-10-20 09:07:58 +0300

    f

--- main.js
+++ main.js
@@ -56,3 +56,118 @@ sel('.testimonials__arrows .arrow--left').addEventListener('click', (e) => {
 sel('.testimonials__arrows .arrow:not(.arrow--left)').addEventListener('click', (e) => {
   testSlider.go('-1')
 })
+
+const presenceSplide = new Splide('.presence__slider', {
+  arrows: false,
+  pagination: false,
+  gap: '3rem',
+  // type: 'loop',
+  perPage: 3,
+  // focus: 'center',
+  // autoWidth: true,
+  breakpoints: {
+    991: {
+      perPage: 2,
+    },
+    767: {
+      perPage: 1,
+    },
+  },
+}).mount()
+
+sel('.presence__arrows .round-arrow--left').addEventListener('click', (e) => {
+  presenceSplide.go('+1')
+})
+sel('.presence__arrows .round-arrow:not(.round-arrow--left)').addEventListener('click', (e) => {
+  presenceSplide.go('-1')
+})
+
+const trendsSplide = new Splide('.trends__slider', {
+  arrows: false,
+  pagination: false,
+  gap: '3rem',
+  perPage: 3,
+  // focus: 'center',
+  // autoWidth: true,
+  breakpoints: {
+    991: {
+      perPage: 2,
+    },
+    767: {
+      perPage: 1,
+      gap: '1rem',
+    },
+  },
+})
+// const Components = trendsSplide.Components
+// // to remove duplicates for inactive/small slider
+// trendsSplide.on('overflow', function (isOverflow) {
+//   trendsSplide.go(0) // Reset the carousel position
+
+//   trendsSplide.options = {
+//     focus: isOverflow ? 'center' : '',
+//     // drag: isOverflow ? 'free' : false,
+//     clones: isOverflow ? undefined : 0, // Toggle clones
+//   }
+// })
+// let s2Overflow = true
+// // to center inactive/small slider
+// trendsSplide.on('resized', function () {
+//   var isOverflow = Components.Layout.isOverflow()
+//   s2Overflow = isOverflow
+//   var list = Components.Elements.list
+//   var lastSlide = Components.Slides.getAt(trendsSplide.length - 1)
+
+//   if (lastSlide) {
+//     // Toggles `justify-content: center`
+//     list.style.justifyContent = isOverflow ? '' : 'center'
+
+//     // Remove the last margin
+//     if (!isOverflow) {
+//       lastSlide.slide.style.marginRight = ''
+//       // console.log('asdf')
+//     }
+//   }
+// })
+if (document.readyState !== 'loading') {
+  initCode()
+} else {
+  document.addEventListener('DOMContentLoaded', function () {
+    initCode()
+  })
+}
+
+function initCode() {
+  trendsSplide.mount()
+}
+
+sel('.trends__slider .round-arrow--left').addEventListener('click', (e) => {
+  trendsSplide.go('+1')
+})
+sel('.trends__slider .round-arrow:not(.round-arrow--left)').addEventListener('click', (e) => {
+  trendsSplide.go('-1')
+})
+
+const bumpSplide = new Splide('.bump__slider', {
+  arrows: false,
+  pagination: false,
+  // gap: '3rem',
+  type: 'loop',
+  perPage: 1,
+  // focus: 'center',
+  // autoWidth: true,
+  breakpoints: {},
+}).mount()
+
+selAll('.bump__arrows .arrow--left').forEach((e) => {
+  e.addEventListener('click', () => {
+    bumpSplide.go('+1')
+    console.log('fd')
+  })
+})
+selAll('.bump__arrows .arrow:not(.arrow--left)').forEach((e) => {
+  e.addEventListener('click', () => {
+    bumpSplide.go('-1')
+    console.log('fd')
+  })
+})
